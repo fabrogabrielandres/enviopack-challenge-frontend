1e5898823fb67cb22d5acbbf22a848750a5cecad
Gabriel Fabro <84206590+fabrogabrielandres@users.noreply.github.com>
2021-11-19 17:51:44 -0600

    ok reducers

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { NavBar } from '../../components/NavBar/NavBar'
+import { LoadProducts } from '../../redux/productsSlice'
 import { Catalogo } from '../Catalogo/Catalogo'
+import products from "../../products.json"
 
 export const Home = () => {
     
+    
+    
+    const dispatch = useDispatch()
+    dispatch(LoadProducts(products.productos))
 
     return (
         <>
--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -3,11 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const productsSlice = createSlice({
     name: "user",
     initialState: {
-        "Products": []
+        "Products": [{
+        }]
     },
     reducers: {
-
+        LoadProducts: (state, action) => {
+            state.Products=  action.payload.map((item) => item = { ...item, isAdd: false })
+        },
+        
     },
 });
-
+export const  { LoadProducts } = productsSlice.actions
 export default productsSlice.reducer;
