7183df5973707ed776d92ac757bf7fb1df9d3766
Gabriel Fabro <84206590+fabrogabrielandres@users.noreply.github.com>
2021-11-22 14:21:30 -0600

    Products + pagination its ok

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,21 +1,68 @@
-import React  from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate'
 import image from "../../assets/image-product.jpg"
 import { CardCatalogo } from '../CardCatalogo/CardCatalogo'
 import { RenderProducts, Wrapper } from './styled'
 
 
-export const Products = ({OrderProducts}) => {
+export const Products = ({ OrderedProducts }) => {
 
-  
+    function Items({ currentItems }) {
+        return (
+            <>
+                {currentItems &&
+                    currentItems.map((item) => (
+                        <CardCatalogo image={image} product={item} />
+                    ))}
+            </>
+        )
+    }
+
+    function PaginatedItems({ itemsPerPage = 6  }) {
+        // We start with an empty list of items.
+        const [currentItems, setCurrentItems] = useState(null);
+        const [pageCount, setPageCount] = useState(0);
+        // Here we use item offsets; we could also use page offsets
+        // following the API or data you're working with.
+        const [itemOffset, setItemOffset] = useState(0);
+
+        useEffect(() => {
+            // Fetch items from another resources.
+            const endOffset = itemOffset + itemsPerPage;
+            console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+            setCurrentItems(OrderedProducts.slice(itemOffset, endOffset));
+            setPageCount(Math.ceil(OrderedProducts.length / itemsPerPage));
+        }, [itemOffset, itemsPerPage]);
+
+        // Invoke when user click to request another page.
+        const handlePageClick = (event) => {
+            const newOffset = (event.selected * itemsPerPage) % OrderedProducts.length;
+            console.log(
+                `User requested page number ${event.selected}, which is offset ${newOffset}`
+            );
+            setItemOffset(newOffset);
+        };
+
+        return (
+            <Wrapper>
+                <RenderProducts>
+                    <Items currentItems={currentItems} />
+                </RenderProducts>
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    renderOnZeroPageCount={null}
+                />
+            </Wrapper>
+        );
+    }
+
     return (
-        <Wrapper>
-             {
-                OrderProducts.map((product, idx) =>
-                    <RenderProducts key={idx}>
-                        <CardCatalogo image={image} product={product} />
-                    </RenderProducts>
-                )
-            }
-        </Wrapper>
+        <PaginatedItems />
     )
+
 }
--- src/components/Products/styled.js
+++ src/components/Products/styled.js
@@ -2,12 +2,16 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     display: flex;
-    flex-wrap: wrap;
+    flex-direction: column;
     margin-top:20px;
     justify-content: space-between;
 
 `
 
 export const RenderProducts = styled.div`
+       display: flex;
+    flex-wrap: wrap;
+    margin-top:20px;
+    justify-content: space-between;
 
 `
